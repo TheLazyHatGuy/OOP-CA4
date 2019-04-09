22496b5721f8f191dc38bbe5de04eb966a06639b
Andrew Carolan <andycarolan9@gmail.com>
2019-04-09 21:51:22 +0100

    Created basic client class
    just a basic setup of objects needed for the client. Server IP added to
    MovieServiceDetails.

--- src/main/java/com/ca4/Client/MovieClient.java
+++ src/main/java/com/ca4/Client/MovieClient.java
@@ -1,9 +1,32 @@
 package com.ca4.Client;
 
+import com.ca4.Core.MovieServiceDetails;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class MovieClient
 {
     public static void main(String[] args)
     {
+        Socket client;
+        OutputStream outputToSocket;
+        InputStream inputFromSocket;
+        PrintWriter streamWriter;
+        BufferedReader streamReader;
+        Scanner scan = new Scanner(System.in);
 
+        try{
+            InetAddress serverIP = InetAddress.getByName(MovieServiceDetails.SERVER_IP);
+
+            client = new Socket(serverIP, MovieServiceDetails.SERVER_PORT);
+        }catch (UnknownHostException ue){
+            ue.printStackTrace();
+        }catch (IOException io){
+            io.printStackTrace();
+        }
     }
 }
--- src/main/java/com/ca4/Core/MovieServiceDetails.java
+++ src/main/java/com/ca4/Core/MovieServiceDetails.java
@@ -3,6 +3,7 @@ package com.ca4.Core;
 public class MovieServiceDetails
 {
     public static final int SERVER_PORT = 50000;
+    public static final String SERVER_IP = "127.0.0.1";
     public static final String BREAKING_CHARACTER = "%%";
 
     //Commands

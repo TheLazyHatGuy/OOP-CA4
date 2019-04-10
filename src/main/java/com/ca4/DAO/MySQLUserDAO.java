930565fd10692d6572892beb599674b3a3cf73d6
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2019-04-10 16:04:36 +0100

    Add login method
    This will allow us to get the user's info based on their email address.

--- src/main/java/com/ca4/DAO/MySQLUserDAO.java
+++ src/main/java/com/ca4/DAO/MySQLUserDAO.java
@@ -1,12 +1,15 @@
 package com.ca4.DAO;
 
+import com.ca4.DTO.User;
 import com.ca4.Exceptions.DAOException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class MySQLUserDAO extends MySQLDAO implements UserDAOInterface {
+public class MySQLUserDAO extends MySQLDAO implements UserDAOInterface
+{
     public boolean registerUser(String email, String password) throws DAOException{
         Connection con = null;
         PreparedStatement ps = null;
@@ -27,6 +30,46 @@ public class MySQLUserDAO extends MySQLDAO implements UserDAOInterface {
                 throw new DAOException("registerUser() " + e.getMessage());
             }
         }
+    }
+
+    public User loginUser (String email) throws DAOException
+    {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        User user = null;
+
+        try
+        {
+            con = this.getConnection();
+            String query = "SELECT * FROM user WHERE email = ? LIMIT 1";
+            ps = con.prepareStatement(query);
+            ps.setString(1, email);
+            rs = ps.executeQuery();
+
+            while (rs.next())
+            {
+                int id = rs.getInt("id");
+                String password = rs.getString("password");
+                user = new User(id, email, password);
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new DAOException("loginUser() " + e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                closeConnection(con, rs, ps);
+            }
+            catch (SQLException e)
+            {
+                throw new DAOException("loginUser() " + e.getMessage());
+            }
+        }
+        return user;
     }
 
     public void deleteUser(int user_id) throws DAOException{
--- src/main/java/com/ca4/DAO/UserDAOInterface.java
+++ src/main/java/com/ca4/DAO/UserDAOInterface.java
@@ -1,11 +1,12 @@
 package com.ca4.DAO;
 
+import com.ca4.DTO.User;
 import com.ca4.Exceptions.DAOException;
 
-public interface UserDAOInterface {
+public interface UserDAOInterface
+{
     boolean registerUser(String email, String password) throws DAOException;
-
+    User loginUser(String email) throws DAOException;
     void deleteUser(int user_id) throws DAOException;
-
     void updateUser(int id, String email, String password) throws DAOException;
 }

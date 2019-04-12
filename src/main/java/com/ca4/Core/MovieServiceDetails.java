b4e80ff3e695e1c58c87b69ae986ef2025e8b137
TheLazyHatGuy <cscholes982@gmail.com>
2019-04-12 21:56:43 +0100

    Make terminal colour public

--- src/main/java/com/ca4/Core/MovieServiceDetails.java
+++ src/main/java/com/ca4/Core/MovieServiceDetails.java
@@ -37,7 +37,7 @@ public class MovieServiceDetails
     public static final CharSequence JSONARRAY_ENDINGCHAR = "]";
 
     //Terminal Colours
-    private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
 }

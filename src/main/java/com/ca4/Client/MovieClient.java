95ee08ab3f00cc5d11f2263e69ecf7d06377540f
Andrew Carolan <andycarolan9@gmail.com>
2019-04-12 09:54:24 +0100

    Login and register added to client class

--- src/main/java/com/ca4/Client/MovieClient.java
+++ src/main/java/com/ca4/Client/MovieClient.java
@@ -1,6 +1,7 @@
 package com.ca4.Client;
 
 import com.ca4.Core.MovieServiceDetails;
+import com.ca4.DTO.User;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -19,12 +20,14 @@ public class MovieClient
     private static BufferedReader streamReader;
     private static Scanner socketInput;
     private static StringBuilder stringBuilder;
+    private static User localUser;
+    private static boolean loggedIn = false;
 
     public static void main(String[] args)
     {
         boolean continueRunning = true;
         String option;
-        boolean loggedIn = true;
+
 
         try{
             InetAddress serverIP = InetAddress.getByName(MovieServiceDetails.SERVER_IP);
@@ -34,6 +37,19 @@ public class MovieClient
             while(continueRunning){
                 if(!loggedIn){
                     option = ClientInteractor.getMenuOption_NotLoggedIn();
+
+                    switch (option){
+                        case "A":
+                            String[] loginDetails = ClientInteractor.loginRegister();
+                            sendUsersDetails(MovieServiceDetails.LOGIN, loginDetails);
+                            break;
+                        case "B":
+                            String[] userDetails = ClientInteractor.loginRegister();
+                            sendUsersDetails(MovieServiceDetails.REGISTER, userDetails);
+                            break;
+                    }
+
+
                 }else if(loggedIn){
                     option = ClientInteractor.getMenuOption_LoggedIn();
 
@@ -50,7 +66,8 @@ public class MovieClient
                             break;
                         case "C":
                             String[] movieGenre = {ClientInteractor.getMovieGenre()};
-                            //sendStringArray();
+                            sendStringArray(MovieServiceDetails.SEARCH_MOVIE_GENRE, movieGenre,
+                                    MovieServiceDetails.JSONARRAY_ENDINGCHAR);
                             break;
                         case "D":
                             //TODO add update movie logic
@@ -85,6 +102,25 @@ public class MovieClient
         }
     }
 
+    public static void sendUsersDetails(String messageCode, String[] details){
+        try{
+            String messageToSend;
+            inputFromSocket = client.getInputStream();
+
+            messageToSend = messageCode;
+
+            for(int i = 0; i < details.length; ++i){
+                messageToSend += MovieServiceDetails.BREAKING_CHARACTER + details[i];
+            }
+
+            streamWriter = new PrintWriter(client.getOutputStream());
+            streamWriter.println(messageToSend);
+            streamWriter.flush();
+            createNewUser(details);
+        }catch (IOException io){
+            io.printStackTrace();
+        }
+    }
 
     public static void sendStringArray(String messageCode, String[] message, CharSequence endingChar){
         try{
@@ -106,11 +142,30 @@ public class MovieClient
         }
     }
 
+    public static void createNewUser(String[] userDetails){
+        Scanner input = new Scanner(new InputStreamReader(inputFromSocket));
+
+        String currentline = "";
+
+        currentline = input.nextLine();
+
+        String[] serverAnswer = currentline.split(MovieServiceDetails.BREAKING_CHARACTER);
+
+        if(serverAnswer[0].equals(MovieServiceDetails.LOGIN_SUCCESS)){
+            localUser = new User(Integer.parseInt(serverAnswer[1]), userDetails[0], userDetails[1]);
+            loggedIn = true;
+            localUser.toString();
+        }else if(serverAnswer[0].equals(MovieServiceDetails.REGISTER_SUCCESS)){
+            sendUsersDetails(MovieServiceDetails.LOGIN, userDetails);
+        }else{
+            System.out.println("Cannot find user on server.");
+        }
+    }
+
     public static void receiveJSONObject(CharSequence endingChar){
             Scanner input = new Scanner(new InputStreamReader(inputFromSocket));
 
             String currentline = "";
-            stringBuilder = new StringBuilder();
 
             while(!(currentline.contains(endingChar))){
                 currentline += input.nextLine();

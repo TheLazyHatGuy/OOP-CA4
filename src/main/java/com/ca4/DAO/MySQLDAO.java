4bd345474da77e8c3261d78e403c7ca44eb3cc35
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2019-04-24 17:26:49 +0100

    Add function to get JSON config file
    This will make it possible to change the database information without
    needing to compile a new JAR file.

--- src/main/java/com/ca4/DAO/MySQLDAO.java
+++ src/main/java/com/ca4/DAO/MySQLDAO.java
@@ -1,27 +1,56 @@
 package com.ca4.DAO;
 
 import com.ca4.Exceptions.DAOException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Scanner;
 
 public class MySQLDAO
 {
+    private String dbHost;
+    private String dbUser;
+    private String dbPass;
+    private String dbName;
+
     public Connection getConnection() throws DAOException
     {
-        String url = "jdbc:mysql://localhost:3306/oopca4";
-        String userName = "root";
-        String password = "";
+        getJSONVariables();
 
+        String url = "jdbc:mysql://" + dbHost + ":3306/" + dbName;
         Connection connection = null;
 
         try{
-            connection = DriverManager.getConnection(url, userName, password);
+            connection = DriverManager.getConnection(url, dbUser, dbPass);
         }
         catch(SQLException ex){
             System.out.println("Connection failed " + ex.getMessage());
             System.exit(1);
         }
         return connection;
+    }
+
+    private void getJSONVariables() {
+        try (Scanner configFile = new Scanner(new BufferedReader(new FileReader("config.json")))) {
+            StringBuilder json = new StringBuilder();
+
+            while (configFile.hasNextLine()) {
+                json.append(configFile.nextLine());
+            }
+
+            JSONObject config = new JSONObject(json.toString());
+
+            dbHost = config.getString("DATABASE_HOST");
+            dbUser = config.getString("DATABASE_USER");
+            dbPass = config.getString("DATABASE_PASS");
+            dbName = config.getString("DATABASE_NAME");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 
     public void closeConnection(Connection con) throws DAOException{

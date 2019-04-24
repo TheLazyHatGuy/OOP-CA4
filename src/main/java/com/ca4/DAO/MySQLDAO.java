bc5c8999e55f303c2a7f22920da6e3503915541e
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2019-04-24 16:00:02 +0100

    Remove unused driver variable

--- src/main/java/com/ca4/DAO/MySQLDAO.java
+++ src/main/java/com/ca4/DAO/MySQLDAO.java
@@ -8,7 +8,6 @@ public class MySQLDAO
 {
     public Connection getConnection() throws DAOException
     {
-        String driver = "com.mysql.cj.jdbc.Driver";
         String url = "jdbc:mysql://localhost:3306/oopca4";
         String userName = "root";
         String password = "";

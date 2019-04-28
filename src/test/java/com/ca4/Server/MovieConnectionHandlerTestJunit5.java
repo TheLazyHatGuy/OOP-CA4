f62739afea187b5f81c041c0db0188733ca36173
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2019-04-28 15:39:05 +0100

    Attempt to upgrade to Junit 5
    This is done to try to get Travis and Circle to build the project.

--- src/test/java/com/ca4/Server/MovieConnectionHandlerTestJunit5.java
+++ src/test/java/com/ca4/Server/MovieConnectionHandlerTestJunit5.java
@@ -1,29 +1,27 @@
 package com.ca4.Server;
 
 import com.ca4.Core.MovieServiceDetails;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(Parameterized.class)
-public class MovieConnectionHandlerTest {
+class MovieConnectionHandlerTestJunit5 {
 
     private static MovieConnectionHandler handler = new MovieConnectionHandler();
     private String input;
     private String expectedResult;
 
-    public MovieConnectionHandlerTest(String input, String expectedResult) {
+    public MovieConnectionHandlerTestJunit5(String input, String expectedResult) {
         this.input = input;
         this.expectedResult = expectedResult;
     }
 
     @Parameterized.Parameters
-    public static Collection<String[]> testConditions() {
+    static Collection<String[]> testConditions() {
         String ironMan = "{\n" +
                 "    \"copies\": 3,\n" +
                 "    \"plot\": \"Tony Stark. Genius, billionaire, playboy, philanthropist. Son of legendary inventor and weapons contractor Howard Stark. When Tony Stark is assigned to give a weapons presentation to an Iraqi unit led by Lt. Col. James Rhodes, he's given a ride on enemy lines. That ride ends badly when Stark's Humvee that he's riding in is attacked by enemy combatants. He survives - barely - with a chest full of shrapnel and a car battery attached to his heart. In order to survive he comes up with a way to miniaturize the battery and figures out that the battery can power something else. Thus Iron Man is born. He uses the primitive device to escape from the cave in Iraq. Once back home, he then begins work on perfecting the Iron Man suit. But the man who was put in charge of Stark Industries has plans of his own to take over Tony's technology for other matters.\",\n" +
@@ -131,7 +129,7 @@ public class MovieConnectionHandlerTest {
     }
 
     @Test
-    public void processRequest() {
+    void processCommand() {
         assertEquals(expectedResult, handler.processCommand(input));
     }
 }

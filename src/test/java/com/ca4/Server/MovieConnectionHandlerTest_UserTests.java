24d1267dd54b8b3a2df7b051d429d50d9b5e6bff
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2019-04-28 23:06:33 +0100

    Attempt to test registering and deleting a user

---
+++ src/test/java/com/ca4/Server/MovieConnectionHandlerTest_UserTests.java
@@ -0,0 +1,73 @@
+package com.ca4.Server;
+
+import com.ca4.Core.MovieServiceDetails;
+import com.ca4.DTO.User;
+import org.junit.jupiter.api.*;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofSeconds;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieConnectionHandlerTest_UserTests {
+
+    private static Lock sequential;
+    private static MovieConnectionHandler handler;
+    private static User user;
+    private static int userID;
+
+    @BeforeAll
+    static void setUp() {
+        sequential = new ReentrantLock();
+        handler = new MovieConnectionHandler();
+        user = new User(0, "test@example.com", "password");
+    }
+
+    //Taken from - https://stackoverflow.com/questions/545194/force-junit-to-run-one-test-case-at-a-time
+    @BeforeEach
+    void setUpEachTest() {
+        sequential.lock();
+    }
+
+    @AfterEach
+    void tearDown() {
+        sequential.unlock();
+    }
+
+    @Test
+    @DisplayName("Register new user")
+    void processCommand_1_RegisterNewUser() {
+        //First test takes a while to initialise cache
+        //This test also takes a long time due to sending an email
+        assertTimeout(ofSeconds(5), () -> {
+            //Taken from - https://stackoverflow.com/questions/15938538/how-can-i-make-a-junit-test-wait
+            await().until(() -> {
+                String response = handler.processCommand(MovieServiceDetails.REGISTER +
+                        MovieServiceDetails.BREAKING_CHARACTER + user.getEmail() +
+                        MovieServiceDetails.BREAKING_CHARACTER + user.getPassword());
+                boolean expectedResult = response.contains(MovieServiceDetails.REGISTER_SUCCESS
+                        + MovieServiceDetails.BREAKING_CHARACTER);
+
+                userID = Integer.parseInt(response.split(MovieServiceDetails.BREAKING_CHARACTER)[1]);
+
+                assertTrue(expectedResult);
+                assertTrue(userID >= 1);
+
+                return true;
+            });
+        });
+    }
+
+    @Test
+    @DisplayName("Delete User")
+    void processCommand_2_DeleteUser() {
+        assertTimeout(ofMillis(50), () -> {
+            String response = handler.processCommand(MovieServiceDetails.DELETE_USER +
+                    MovieServiceDetails.BREAKING_CHARACTER + userID);
+            assertEquals(MovieServiceDetails.DELETE_USER, response);
+        });
+    }
+}
